597ce32fcbc0307ccd3c2611716cd714fe562e81
Tanbir Jishan <tantalising007@gmail.com>
2026-02-03 17:51:14 +0000

    only consider the login attempts within a specified time period

--- src/main.py
+++ src/main.py
@@ -1,12 +1,14 @@
 import os
 import time
+import asyncio
 from typing import Generator
 from parser import get_login_info
 
 LOG_PATH = "test.log" #"/var/log/auth.log"
 HIGHEST_TRY = 5
+WAITING_PERIOD = 5
 
-def follow_file(filepath):
+async def follow_file(filepath):
     if not os.path.exists(filepath):
         print(f"Error: Log file not found at {filepath}")
         return
@@ -17,24 +19,38 @@ def follow_file(filepath):
         while True:
             line = f.readline()
             if not line:
-                time.sleep(0.5) # let the cpu take some rest.
-                continue
+               await asyncio.sleep(0.5) # let the cpu take some rest.
+               continue
             yield line
 
 def print_if_too_many_attempt(current_try, highest_try, ip):
     if current_try >= highest_try:
         print(f'ALERT! Brute force attempt detected from ip: {ip}')
 
+
+async def start_clock(seconds, attempts):
+    while True:
+        print('clock started')
+        await asyncio.sleep(seconds)
+        print("clearing attempts")
+        attempts.clear()
+
+
 failed_attempts = {}
 
-for line in follow_file(LOG_PATH):
-    match = get_login_info(line)
-    if match:
-        ip = match['ip']
-        if ip in failed_attempts:
-            failed_attempts[ip] += 1
-            print_if_too_many_attempt(failed_attempts[ip], HIGHEST_TRY, ip)
-        else:
-            failed_attempts[ip] = 1
+async def main():
+    asyncio.create_task(start_clock(WAITING_PERIOD, failed_attempts))
+    async for line in follow_file(LOG_PATH):
+        match = get_login_info(line)
+        if match:
+            ip = match['ip']
+            if ip in failed_attempts:
+                failed_attempts[ip] += 1
+                print_if_too_many_attempt(failed_attempts[ip], HIGHEST_TRY, ip)
+            else:
+                failed_attempts[ip] = 1
 
-print(f"status is {failed_attempts}")
+    print(f"status is {failed_attempts}")
+
+if __name__ == "__main__":
+    asyncio.run(main())
